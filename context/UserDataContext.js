b6fa36302878c7baa88db7e390a097aa005f376a
Sayan Sarkar <kingsarkar3006@gmail.com>
2023-02-25 21:39:04 +0530

    firebase setup done

---
+++ context/UserDataContext.js
@@ -0,0 +1,24 @@
+
+import React, { useContext, useState } from "react";
+import { createContext } from "react";
+
+const defaultValues = {
+  userData: null,
+  setUserData: () => {},
+};
+
+const userDataContext = createContext(defaultValues);
+
+export function useUser() {
+  return useContext(userDataContext);
+}
+
+export function userDataContextProvider({ children }) {
+  const [userData, setUserData] = useState(defaultValues.userData);
+
+  return (
+    <userDataContext.Provider value={{ userData, setUserData }}>
+      {children}
+    </userDataContext.Provider>
+  );
+}
